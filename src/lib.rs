502399ee5b487fdb49bfd87d0e91ddf91437a8c0
David Benjamin <db@iamdb.co>
2022-11-23 15:51:38 -0800

    make main.rs minimal move start to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+use crate::{
+    games::{Game, SupportedGames},
+    stream::VideoStream,
+};
 use clap::Parser;
+use opencv::core::{get_num_threads, set_num_threads};
+use std::thread::available_parallelism;
 
 extern crate pretty_env_logger;
 #[macro_use]
@@ -31,4 +37,44 @@ pub struct Config {
 
     #[clap(long, default_value_t = 2)]
     pub process_frame_rate: i64,
+}
+
+pub async fn start(game: SupportedGames) {
+    pretty_env_logger::init_timed();
+
+    let mut config = Config::parse();
+
+    if config.num_opencv_threads == 0 {
+        let total_threads = available_parallelism().unwrap().get() as i32 / 2;
+        config.num_opencv_threads = total_threads;
+    }
+
+    if !opencv::core::use_optimized().expect("error checking for optimized code") {
+        debug!("changing opencv to use optimized code");
+        opencv::core::set_use_optimized(true).expect("error enabling optimized code");
+    } else {
+        debug!("opencv is using optimized code")
+    }
+
+    set_num_threads(config.num_opencv_threads / 2).unwrap();
+
+    let opencv_threads = get_num_threads().expect("error retrieving number of threads");
+    debug!("opencv is using {} threads", opencv_threads);
+
+    let pipe = crate::pipeline::new();
+    if config.show_frames {
+        pipe.start_preview_thread();
+    }
+
+    let game = crate::games::new(game);
+    let regions = game.regions();
+
+    for i in 0..config.num_opencv_threads / 2 {
+        pipe.process_thread(i as i32, config.show_frames, regions.clone())
+            .await;
+    }
+
+    let decoder_sender = pipe.get_decode_sender();
+    let mut stream = VideoStream::new(config, decoder_sender);
+    stream.decode().await
 }
